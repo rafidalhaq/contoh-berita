c2b9b58a365cf05d0007f91ca7a9d6eea7f91dbc
Ghuzandi Rafid A <rafid.alhaq@gmail.com>
2021-01-26 17:28:16 +0700

    backend setengah

---
+++ application/controllers/Post_berita.php
@@ -0,0 +1,77 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Post_berita extends CI_Controller {
+
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_berita');
+        $this->load->model('upload');
+    
+    }
+    
+    function index()
+    {
+        $this->load->view('v_post_news');
+            
+    }
+
+    function simpan_post()
+    {
+        $config['upload_path'] = './assets/images/';
+        $config['allowed_types'] = 'git|jpg|png|jpeg|bmp';
+        $config['encrypt_name'] = TRUE;
+
+        $this->upload->initialiaze($config);
+        if (!empty($_FILES['filefoto']['name'])) {
+            if ($this->upload->do_upload('filefoto')) {
+                $gbr =  $this->upload->data();
+                //compress Image
+                $config['image_library'] = 'gd2';
+                $config['source_image'] = './assets/images/'.$gbr['file_name'];
+                $config['create_thumb'] = 'FALSE';
+                $config['maintain_ratio'] = 'FALSE';
+                $config['quality'] = '60%';
+                $config['width'] = '710';
+                $config['height'] = '420';
+                $config['new_image'] = './assets/images/'.$gbr['file_name'];
+                $this->load->library('image_lib', $config);
+                $this->image_lib->resize();
+
+                $gambar = $gbr['file_name'];
+                $jdl = $this->input->post('berita');
+
+                $this->m_berita->simpan_berita($jdl,$berita,$gambar);
+                redirect('post_berita/lists');
+                
+            }else{
+                redirect('post_berita');
+            }
+        }else{
+            redirect('post_berita');    
+        }
+        
+    }
+
+    function lists()
+    {
+        $x['data']=$this->m_berita->get_all_berita();
+        $this->load->view('v_post_list', $x);
+        
+    }
+
+    function view()
+    {
+        $kode=$this->uri->segment(3);
+        $x['data']=$this->m_berita->get_berita_by_kode($kode);
+        $this->load->view('v_post_view', $x);
+        # code...
+    }
+
+}
+
+/* End of file Post_berita.php */
+
---
+++ application/models/M_berita.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_berita extends CI_Model {
+
+    function simpan_berita($jdl,$berita,$gambar)
+    {
+        $hsl=$this->db->query("INSERT INTO tbl_berita (berita_judul, berita_isi,berita_image) VALUES ('$jdl','$berita','$gambar')");
+        return $hsl;
+        # code...
+    }
+
+    function get_berita_by_kode($kode)
+    {
+        $hsl=$this->db->query("SELECT * FROM tbl_berita WHERE berita_id='$kode'");
+        return $hsl;
+        
+    }
+
+    function get_all_berita()
+    {
+        $hsl=$this->db->query("SELECT * FROM tbl_berita ORDER BY berita_id DESC");
+        return $hsl;
+    }
+
+}
+
+/* End of file M_berita.php */
